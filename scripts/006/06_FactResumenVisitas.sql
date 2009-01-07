5c8c86c9b2d53e4da8a412f81637caf15dcaaf1e
sergioemv <sergioemv@bcea3d96-dacf-11dd-8ca8-e3ff79f713b6>
2009-01-07 16:38:57 +0000

    inicio de Fact resumen visitas

---
+++ scripts/006/06_FactResumenVisitas.sql
@@ -0,0 +1,25 @@
+create table Fact_ResumenVisitasPalm(
+   empresaSucursalAgenciaFK int not null 
+     foreign key references Dim_EmpresaSucursalAgencia(EmpresaSucursalAgenciaId),
+    fechafk int not null foreign  key references Dim_Fecha(fechaid),
+    clientefk int not null foreign key references Dim_Clientes(ClienteId),
+    vendendorFk int not null foreign key references Dim_Vendedor(vendedorId),
+    distritoRutaSectorFK int not null foreign key references Dim_DistritoRutaSector(distritoRutaSectorId),
+    cantPuntosActivos int,
+    cantPuntosPotenciales int,
+    cantPuntosVisitados int,
+    cantPuntosPrecompra int,
+    cantClientesPotVisitados int,
+    cantClientesPotConvert int,
+    altasPotenciales int,
+    altasActivos int,
+    cajasPrevendidas numeric(7,2),
+    cantClientesBaja int,
+    cantConExibicion int,
+    cantUtilizacionFrio int,
+    cantMarcaPrecio int,
+    cantAfiche int,
+    cantOtros int
+)
+
+

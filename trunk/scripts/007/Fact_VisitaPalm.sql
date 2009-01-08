05b224f4d75b145122b4476f34f6efa99c7eacd7
sergioemv <sergioemv@bcea3d96-dacf-11dd-8ca8-e3ff79f713b6>
2009-01-08 21:03:31 +0000

    inicio visita palm cubo resumen visitas palm

---
+++ trunk/scripts/007/Fact_VisitaPalm.sql
@@ -0,0 +1,15 @@
+drop table fact_visitaPalm
+go
+create table Fact_VisitaPalm
+(
+  fechafk int not null foreign key references Dim_fecha (fechaid),
+  horaVisfk int not null foreign key references Dim_Hora(horaid),
+  horaRevfk int not null foreign key references Dim_Hora(horaid),
+  clientefk int not null foreign key references Dim_Clientes(clienteid),
+  empresaSucursalAgenciafk int not null foreign key references Dim_EmpresaSucursalAgencia(EmpresaSucursalAgenciaId),
+  vendedorfk int not null foreign key references Dim_Vendedor(vendedorid),
+  actividadVisitaFk int not null foreign key references Dim_ActividadVisita(actividadVisitaId),
+  estadoVisita numeric(2,0),
+  nroVisitaDD numeric(7,0) not null, 
+  primary key clustered (fechafk,empresaSucursalAgenciafk,vendedorfk, nroVisitaDD)
+)

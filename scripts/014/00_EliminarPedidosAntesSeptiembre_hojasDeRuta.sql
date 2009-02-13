a168ebba90458c715a7194f5ddc1d626e6a9cff1
sergioemv <sergioemv@bcea3d96-dacf-11dd-8ca8-e3ff79f713b6>
2009-02-13 02:23:16 +0000

    1. Cantidad de clientes en hojas de ruta, numero incorrecto 2. Mostrar el codigo de cliente en lugar del correlativo 3. Mostrar el codigo del vendedor en lugar del correlativo 4. Correccion en el calculo del descuento de los pedidos 5. Mostrar cantidad de cajas objetivo en lugar de articulos objetiv 6. Crear un perfil para obtencion de cubos locales

--- scripts/014/00_EliminarPedidosAntesSeptiembre_hojasDeRuta.sql
+++ scripts/014/00_EliminarPedidosAntesSeptiembre_hojasDeRuta.sql
@@ -1,5 +1,7 @@
 delete from Fact_HojaDeRuta
+where clienteFK in ( select clienteId from Dim_Clientes where idClienteMendo in(
+select CNRCLI from MDV020_Clientes where CTIPCLIA<>'A'))
 go
 delete from Fact_Pedidos
-where fechaFK < (select Fechaid from Dim_Fecha where fecha = '01/09/2008')
+where fechaFK < (select Fechaid from Dim_Fecha where fecha = '2008/09/01')
 go
